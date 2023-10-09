919239a26a69a33dbdfe8c66e443e11ec20e2d08
rsaf <rsaf@rsafs-MacBook-Pro.local>
2023-10-09 23:49:15 +0800

    feat: made card css for cities and attractions

--- src/components/AttractionCard/AttractionCard.jsx
+++ src/components/AttractionCard/AttractionCard.jsx
@@ -2,17 +2,26 @@ import { Link } from "react-router-dom";
 // import ReviewPage from "../../pages/ReviewPage/ReviewPage";
 
 const AttractionCard = ({ cityInfo, attraction }) => {
-  console.log(cityInfo);
   const city = cityInfo._id;
-  console.log(city);
 
   return (
     <div>
       <Link to={`/bucketlist/${city}/${attraction._id}`}>
-        <p>Attraction Name: {attraction.name}</p>
-        <p>Attraction Description: {attraction.description}</p>
-        <p>Attraction Id: {attraction._id}</p>
-        {console.log(attraction._id)}
+        <div className="card mt-8 w-96 bg-base-100 shadow-xl image-full">
+          <figure>
+            <img
+              src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+              alt={attraction.name}
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Attraction Name: {attraction.name}</h2>
+            <p>Attraction Description: {attraction.description}</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Remove from list</button>
+            </div>
+          </div>
+        </div>
         {/* {attraction.map((reviews) => (
           <ReviewPage key={attraction._id} reviews={reviews} />
         ))} */}
--- src/pages/AttractionPage/AttractionPage.jsx
+++ src/pages/AttractionPage/AttractionPage.jsx
@@ -4,28 +4,35 @@ import AttractionCard from "../../components/AttractionCard/AttractionCard";
 const AttractionPage = ({ cities }) => {
   const { cityId } = useParams();
   const cityInfo = cities?.find((city) => city._id === cityId);
-  console.log(cityInfo);
 
   return (
     <div>
-      <h1>Attraction Page</h1>
+      <h1 className="flex justify-center">Attraction Page</h1>
       {cityInfo ? (
-        <div>
-          <h2>{cityInfo.name}</h2>
-          <p>{cityInfo.description}</p>
-          <div>
-            {cityInfo.attractions.map((attraction) => (
-              <AttractionCard 
-                key={attraction._id}
-                attraction={attraction}
-                cityInfo={cityInfo}
-              />
-            ))}
+        <div className="card mt-3 w-98 bg-base-100 shadow-xl image-full">
+          <figure>
+            <img
+              src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">City Name: {cityInfo.name}</h2>
+            <p>City Description: {cityInfo.description}</p>
           </div>
         </div>
       ) : (
-        <p>City not found</p>
+        <p>City Not found</p>
       )}
+      <div className="grid justify-center">
+        {cityInfo?.attractions?.map((attraction) => (
+          <AttractionCard
+            key={attraction._id}
+            attraction={attraction}
+            cityInfo={cityInfo}
+          />
+        ))}
+      </div>
     </div>
   );
 };
