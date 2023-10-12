133b1a798b67a4b682d0e22161b1027ce70a6771
rsaf <rsaf@rsafs-MacBook-Pro.local>
2023-10-12 16:06:31 +0800

    style: standardised the colour of the buttons

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -18,26 +18,30 @@ export default function NavBar({ user, setUser, hideSearchResults }) {
 
   return (
     <nav>
-      <div className="navbar bg-base-100 bg-indigo-800 text-white h-14 flex justify-between">
+      <div className="navbar bg-indigo-800 text-white h-14 flex justify-between">
         <div className="flex">
           <button className="font-semibold" onClick={() => handleNavigate("/")}>
-            <span className="text-yellow-500 text-2xl">Travel</span>
-            {" "}
-            <span className="text-blue-500 text-2xl">Log</span>
+            <span className="text-info text-xl">Travel</span>{" "}
+            <span className="text-accent text-xl">Log</span>
           </button>
           <div className="mx-8">
             <Link to="/bucketlist" className="flex items-center">
               <span className="mr-1">
                 <BsFillBasket3Fill />
               </span>
-              <span>My Bucket List</span>
+              <span className="text-accent">My Bucket List</span>
             </Link>
           </div>
         </div>
 
         <div className="flex">
           <p className="text-lg mx-5">Hi, {user.name}</p>
-          <button className="btn btn-sm btn-outline btn-accen" onClick={handleLogout}>Logout</button>
+          <button
+            className="btn btn-sm btn-outline btn-accent"
+            onClick={handleLogout}
+          >
+            Logout
+          </button>
         </div>
       </div>
     </nav>
